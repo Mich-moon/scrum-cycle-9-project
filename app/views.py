cfa2622c0350db94bf5a63e5efdfd46bcf48b9a9
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-06-26 19:07:42 -0500

    Updated Starter code

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from .models import User
 
 #home page
 @app.route('/')
-def hello_world():
+def home():
     # example without a template
     return 'Hello, World!'
 
@@ -21,6 +21,9 @@ def about():
 """
 The user enters their email and password
 """
+@app.route('/signup')
+def signup ():
+  return 
 
 #login page
 """
@@ -30,23 +33,31 @@ The role (e.g. an admin user or regular user) , created_at an userid will be aut
 Users can be appointed as an admin by the Main Admin.
 Send a request to be an admin. If approve then the user status must be chnaged to admin, otherwise they remain as a regular user
 """
-
+@app.route('/login')
+def login ():
+  return 
 
 #normal user page
 """
   Perform the CRUD operations on the user page
   if we feel good we can add a calendar 
 """
+@app.route('/profile/<userid>')
+def user_page ():
+  return 
 
 
 #admin user page
 """
-  Perform the RUD operations on the user page (Read, update event status and delete events)
- They should have a masterlist of all events
- They can filter by status 
- Add admin username beside the even they approved. 
-    On the events_table there should be a column called approved by [String] and approved at [date]
+Perform the RUD operations on the user page (Read, update event status and delete events)
+They should have a masterlist of all events
+They can filter by status 
+Add admin username beside the even they approved. 
+  On the events_table there should be a column called approved by [String] and approved at [date]
 """
+@app.route('/admin/<userid>')
+def admin_page ():
+  return 
 
 
 @app.route('/profile/<username>')
