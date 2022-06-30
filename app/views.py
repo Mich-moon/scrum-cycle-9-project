7a646eb57713f8d20e01f55d2414d7b333a5335b
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-06-30 02:02:30 -0500

    updated routes

--- app/views.py
+++ app/views.py
@@ -7,9 +7,9 @@ from app import app, db
 from .forms import EventsForm, SignupForm, LoginForm
 from .models import User, Event
 from werkzeug.security import check_password_hash
-#from app import jwt
-#from flask_jwt_extended import create_access_token
-#from flask_jwt_extended import jwt_required
+from app import jwt
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
 
 # home page
 
@@ -54,8 +54,20 @@ def signup():
         db.session.add(user)
         db.session.commit()
 
+        new_id = user.id
+        user = db.session.query(User).get(new_id)
+
+        user_json = {
+            "id": user.id,
+            "full_name": user.full_name,
+            "email": user.email,
+            "photo": user.profile_photo,
+            "role": user.role,
+            "created_at": user.created_at
+        }
+
         flash('Signup successful', 'success')
-        return jsonify(message="Signup Successful"), 201
+        return jsonify(user=user_json), 201
 
     return jsonify(message="Signup Failed", errors=form_errors(form)), 400
 
@@ -109,7 +121,7 @@ def login():
         else:
             flash('Invalid email or password', 'error')
 
-    return jsonify(message="Login Failed")
+    return jsonify(message="Login Failed", errors=form_errors(form)), 400
 
 
 @app.route('/api/users/<user_id>', methods=["GET"])
@@ -125,7 +137,7 @@ def get_user(user_id):
                 "id": user.id,
                 "full_name": user.full_name,
                 "email": user.email,
-                "photo": user.photo,
+                "photo": user.profile_photo,
                 "role": user.role,
                 "created_at": user.created_at
             }
@@ -135,12 +147,18 @@ def get_user(user_id):
         return jsonify(message="Item not found"), 404
 
 
-@app.route('/api/users/<user_id>/events', methods=["GET"])
+@app.route('/events', methods=["GET", "POST"])
+def event_page():
+    form = EventsForm()
+    return render_template('create_event_form.html', form=form)
+
+
+@app.route('/api/users/<user_id>/events', methods=["GET", "POST"])
 def get_user_events(user_id):
-    """Get Details of an event for a user"""
-    user_events = db.session.query(Event).filter_by(uid=user_id).all()
+    """Get Details of an event for a user ane create event"""
 
     if request.method == 'GET':
+        user_events = db.session.query(Event).filter_by(uid=int(user_id)).all()
 
         if user_events is not None:
 
@@ -163,6 +181,63 @@ def get_user_events(user_id):
 
         return jsonify(message="Item not found"), 404
 
+    elif request.method == 'POST':
+
+        form = EventsForm(obj=request.form)
+
+        if form.validate_on_submit():
+            title = form.title.data
+            start_date = form.start_date.data
+            end_date = form.end_date.data
+            description = form.description.data
+            venue = form.venue.data
+            flyer = form.flyer.data
+            website = form.website.data
+            flyer_filename = secure_filename(flyer.filename)
+            flyer.save(os.path.join(
+                os.environ.get('UPLOAD_FOLDER'), flyer_filename
+            ))
+
+            event = Event(
+                title=title,
+                start_date=start_date,
+                end_date=end_date,
+                description=description,
+                venue=venue,
+                flyer=flyer_filename,
+                website=website,
+                uid=int(user_id),
+                updated_at=datetime.datetime.utcnow()  # need to work on this
+            )
+
+            db.session.add(event)
+            db.session.commit()
+            # app.logger.debug(full_name)
+            flash("Event Successfully Created")
+            # return redirect(url_for('home'))
+
+            new_id = event.id
+            event = db.session.query(Event).get(new_id)
+
+            event_json = {
+                "id": event.id,
+                "title": event.title,
+                "start_date": event.start_date,
+                "end_date": event.end_date,
+                "description": event.description,
+                "venue": event.venue,
+                "flyer": event.flyer,
+                "website": event.website,
+                "status": event.status,
+                "uid": event.uid,
+                "created_at": event.created_at,
+                "updated_at": event.updated_at
+            }
+
+            return jsonify(event=event_json), 201
+
+        return jsonify(message="Event creation Failed", errors=form_errors(form)), 400
+
 
 @app.route('/api/users/<user_id>/events/<event_id>', methods=["PUT", "DELETE"])
 def edit_user_events(user_id):
@@ -171,38 +246,36 @@ def edit_user_events(user_id):
 
     if request.method == 'PUT':
         if user_event is not None:
+            request_data = request.get_json()
+            Event.update_event(
+                id,
+                request_data["title"],
+                request_data["start_date"],
+                request_data["end_date"],
+                request_data["description"],
+                request_data["venue"],
+                request_data["flyer"],
+                request_data["website"],
+                request_data["updated_at"]
+            )
+            event = db.session.query(Event).get(int(event_id))
 
-            form = EventsForm(obj=request.form)
+            event_json = {
+                "id": e.id,
+                "title": e.title,
+                "start_date": e.start_date,
+                "end_date": e.end_date,
+                "description": e.description,
+                "venue": e.venue,
+                "flyer": e.flyer,
+                "website": e.website,
+                "status": e.status,
+                "uid": e.uid,
+                "created_at": e.created_at,
+                "updated_at": e.updated_at
+            }
 
-            if form.validate_on_submit():
-                title = form.title.data
-                start_date = form.start_date.data
-                end_date = form.end_date.data
-                description = form.description.data
-                venue = form.venue.data
-                flyer = form.flyer.data
-                website = form.website.data
-                flyer_filename = secure_filename(flyer.filename)
-                flyer.save(os.path.join(
-                    os.environ.get('UPLOAD_FOLDER'), flyer_filename
-                ))
-
-                event = Event(
-                    title=title,
-                    start_date=start_date,
-                    end_date=end_date,
-                    description=description,
-                    venue=venue,
-                    flyer=flyer_filename,
-                    website=website,
-                    uid=user_id,
-                    date_updated=datetime.datetime.utcnow()  # need to work on this
-                )
-
-                db.session.update(event)
-                db.session.commit()
-
-            return jsonify(message="Event updated"), 200
+            return jsonify(message="Event updated", event=event_json), 200
 
         return jsonify(message="Item not found"), 404
 
@@ -216,6 +289,8 @@ def edit_user_events(user_id):
 
         return jsonify(message="Item not found"), 404
 
+    return jsonify(message=""), 400
+
 
 # admin user page - sr
 """
@@ -227,10 +302,11 @@ Add admin username beside the even they approved.
 """
 
 
-#@app.route('/admin/<userid>')
-@app.route('/admin', methods = ['GET'])
+# @app.route('/admin/<userid>')
+@app.route('/admin', methods=['GET'])
 def admin_page():
     return jsonify({'Events': Event.get_all_events()})
+
 
 '''@app.route('/admin', methods=['POST'])
 def add_event():
@@ -239,71 +315,36 @@ def add_event():
     response = Response("Event added", status=201, mimetype='application/json')
     return response'''
 
+
 @app.route('/admin/<int:id>', methods=['GET'])
 def get_event_by_id(id):
     return_value = Event.get_event(id)
     return jsonify(return_value)
 
+
 @app.route('/admin/<int:id>', methods=['PUT'])
 def update_event(id):
     request_data = request.get_json()
-    Event.update_event(id,request_data["title"], request_data["start_date"],request_data["end_date"],request_data["description"],request_data["venue"],request_data["flyer"],request_data["website"],request_data["updated_at"])
-    response = Response("Event Updated", status=200, mimetype='application/json')
+    Event.update_event(id, request_data["title"], request_data["start_date"], request_data["end_date"], request_data["description"],
+                       request_data["venue"], request_data["flyer"], request_data["website"], request_data["updated_at"])
+    response = Response("Event Updated", status=200,
+                        mimetype='application/json')
     return response
+
 
 @app.route('/admin/<int:id>', methods=['DELETE'])
 def remove_event(id):
     Event.delete_movie(id)
-    response = Response("Event Deleted", status=200, mimetype='application/json')
+    response = Response("Event Deleted", status=200,
+                        mimetype='application/json')
     return response
+
 
 # normal user page - ag&mm
 """
   Perform the CRUD operations on the user page
   if we feel good we can add a calendar 
 """
-
-
-@app.route('/events', methods=["GET", "POST", "DELETE", "PUT"])
-def events():
-    form = EventsForm()
-
-    if form.validate_on_submit():
-        title = form.title.data
-        start_date = form.start_date.data
-        end_date = form.end_date.data
-        description = form.description.data
-        venue = form.venue.data
-        flyer = form.flyer.data
-        website = form.website.data
-        flyer_filename = secure_filename(flyer.filename)
-        flyer.save(os.path.join(
-            os.environ.get('UPLOAD_FOLDER'), flyer_filename
-        ))
-
-        event = Event(
-            title=title,
-            start_date=start_date,
-            end_date=end_date,
-            description=description,
-            venue=venue,
-            flyer=flyer_filename,
-            website=website,
-            uid=1,  # need to work on this
-            date_updated=datetime.datetime.utcnow()  # need to work on this
-        )
-
-        db.session.add(event)
-        db.session.commit()
-        # app.logger.debug(full_name)
-        flash("Event Successfully Created")
-        return redirect(url_for('home'))
-
-    for error in form.errors:
-        app.logger.error(error)
-        flash(error)
-
-    return render_template('create_event_form.html', form=form)
 
 
 @app.route('/profile')
