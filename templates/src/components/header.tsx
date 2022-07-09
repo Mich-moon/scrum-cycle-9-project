6d869d15e0738eb30133a95716bbd4d7aa7da94b
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-09 11:20:24 -0500

    Updated header for unauthorised users

--- templates/src/components/header.tsx
+++ templates/src/components/header.tsx
@@ -6,14 +6,14 @@ interface ContainerProps { }
 const header: React.FC<ContainerProps> = () => {
   return (    
         <IonToolbar>
-            <IonButtons id='home-btns'>
-                <IonButton slot='primary' color='primary' id='home-btn1' href='/home' target='_self'>
+            <IonButtons id='home-btns' class='ion-justify-content-end'>
+                <IonButton id='home-btn1' color='primary' href='/home' target='_self'>
                     Home
                 </IonButton>
-                <IonButton  slot='primary' fill='outline' color='primary' id='home-btn2' href='./Signup' target='_self'>
+                <IonButton id='home-btn2' fill='outline' color='primary' href='./Signup' target='_self'>
                     Signup
                 </IonButton>
-                <IonButton  slot='primary' fill='solid' color='primary' id='home-btn3' href='./Login' target='_self'>
+                <IonButton id='home-btn3' fill='solid' color='primary' href='./Login' target='_self'>
                     Login
                 </IonButton>
             </IonButtons>
