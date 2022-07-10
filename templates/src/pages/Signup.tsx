f3d89080d5edb633d47910d28392f07264b20ba4
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-10 16:05:35 -0500

    Updated Signup page

---
+++ templates/src/pages/Signup.tsx
@@ -0,0 +1,46 @@
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonLabel, IonInput, IonItemDivider, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import Header from '../components/header';
+import './Signup.css';
+
+const Signup: React.FC = () => {
+  return (
+    <IonPage>
+        <IonHeader className='ion-no-border'>
+            <IonToolbar>
+                <Header />
+            </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        {/*   <IonHeader collapse='condense'>
+                <IonToolbar>
+                    <IonTitle size='large'>Blank2</IonTitle>
+                </IonToolbar>
+            </IonHeader> */}
+
+            <IonGrid className='ion-margin' id='signup-grid'>
+                <IonRow></IonRow>
+                <IonRow>
+                    <IonCol></IonCol>
+                    <IonCol id='signup-col2'>
+                        <IonTitle className='ion-padding-bottom' id='signup-title'>SIGNUP</IonTitle>
+                        <IonInput placeholder='Firstname' clearInput></IonInput>
+                        <IonInput placeholder='Lastname' clearInput></IonInput>
+                        <IonInput type='email' placeholder='Email Address' clearInput></IonInput>
+                        <IonInput type='password' placeholder='Password' clearInput></IonInput>
+                        <IonInput type='password' placeholder='Confirm Password' clearInput></IonInput>
+                        <IonItemDivider id='signup-photo-upload'>
+                            <IonLabel color='primary'>Add a Profile Photo</IonLabel>
+                            <input type='file' id='imginput' accept='image/*'/>
+                        </IonItemDivider>                        
+                        <IonButton id='signup-btn' fill='solid' color='primary'>Create Account</IonButton>           
+                    </IonCol>
+                    <IonCol></IonCol>
+                </IonRow>
+                <IonRow></IonRow>
+            </IonGrid>
+        </IonContent>
+    </IonPage>
+  );
+};
+
+export default Signup;
