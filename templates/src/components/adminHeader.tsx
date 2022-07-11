09b3364d9a9ccd364a88fe49688e0488c9866146
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 04:36:17 -0500

    Added the admin header and css
    Add the header for admin view

--- templates/src/components/adminHeader.tsx
+++ templates/src/components/adminHeader.tsx
@@ -4,17 +4,18 @@ import { IonToolbar, IonButtons, IonButton } from '@ionic/react';
 interface ContainerProps { }
 
 const adminHeader: React.FC<ContainerProps> = () => {
-  return (    
-        <IonToolbar color='primary'>
-            <IonButtons id='home-btns' class='ion-justify-content-end'>
-                <IonButton id='home-btn1' color='primary' href='/home' target='_self'>
-                    Home
+  return (
+    <IonToolbar color='primary'>
+            <IonButtons id='admin-btns' class='ion-justify-content-end'>
+                <IonButton id='admin-btn1' href='./main' target='_self'>
+                    Main
                 </IonButton>
-                <IonButton id='home-btn2'  color='primary' href='./Signup' target='_self'>
-                    Signup
+                <IonButton id='admin-btn2' href='./home' target='_self'>
+                    Profile
                 </IonButton>
-                <IonButton id='home-btn3' fill='solid' color='primary' href='./Login' target='_self'>
-                    Login
+                {/* hover over profile button and it gives the name of the user*/}
+                <IonButton id='admin-btn3' fill='outline' href='./home' target='_self'>
+                    Logout
                 </IonButton>
             </IonButtons>
         </IonToolbar>
