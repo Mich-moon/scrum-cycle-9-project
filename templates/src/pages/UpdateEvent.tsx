a36684b09d49866c46688d4d86ec7e5d6c182803
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 15:23:38 -0500

    Update update event

--- templates/src/pages/UpdateEvent.tsx
+++ templates/src/pages/UpdateEvent.tsx
@@ -22,19 +22,6 @@ const UpdateEvent: React.FC = () => {
                 <IonRow>
                     <IonCol></IonCol>
                     <IonCol id='update-col2'>
-<<<<<<< Updated upstream
-                        <IonTitle className='ion-padding-bottom' id='update-title'>UPDATE EVENT <i>to be updated</i></IonTitle>
-                        <IonInput placeholder='Firstname' clearInput></IonInput>
-                        <IonInput placeholder='Lastname' clearInput></IonInput>
-                        <IonInput type='email' placeholder='Email Address' clearInput></IonInput>
-                        <IonInput type='password' placeholder='Password' clearInput></IonInput>
-                        <IonInput type='password' placeholder='Confirm Password' clearInput></IonInput>
-                        <IonItemDivider id='update-photo-upload'>
-                            <IonLabel color='primary'>Add a Profile Photo</IonLabel>
-                            <input type='file' id='imginput' accept='image/*'/>
-                        </IonItemDivider>                        
-                        <IonButton id='update-btn' fill='solid' color='primary'>Create Account</IonButton>           
-=======
                         <IonTitle className='ion-padding-bottom' id='update-title'>UPDATE EVENT</IonTitle>
                         <IonInput value='Title' clearInput></IonInput>
                         <IonInput value='Description' clearInput></IonInput>
@@ -57,7 +44,6 @@ const UpdateEvent: React.FC = () => {
                             <IonButton id='update-btn' fill='solid' color='primary'>Update</IonButton>    
                             <IonButton id='update-btn' fill='solid' color='primary'>Delete</IonButton>     
                         </IonButtons>       
->>>>>>> Stashed changes
                     </IonCol>
                     <IonCol></IonCol>
                 </IonRow>
