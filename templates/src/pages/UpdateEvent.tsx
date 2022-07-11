b4eab3add5b76e13f66e477fd68213c9da5941e9
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 11:03:09 -0500

    update update events page

--- templates/src/pages/UpdateEvent.tsx
+++ templates/src/pages/UpdateEvent.tsx
@@ -17,22 +17,22 @@ const UpdateEvent: React.FC = () => {
                 </IonToolbar>
             </IonHeader> */}
 
-            <IonGrid className='ion-margin' id='signup-grid'>
+            <IonGrid className='ion-margin' id='update-grid'>
                 <IonRow></IonRow>
                 <IonRow>
                     <IonCol></IonCol>
-                    <IonCol id='signup-col2'>
-                        <IonTitle className='ion-padding-bottom' id='signup-title'>UPDATE EVENT <i>to be updated</i></IonTitle>
+                    <IonCol id='update-col2'>
+                        <IonTitle className='ion-padding-bottom' id='update-title'>UPDATE EVENT <i>to be updated</i></IonTitle>
                         <IonInput placeholder='Firstname' clearInput></IonInput>
                         <IonInput placeholder='Lastname' clearInput></IonInput>
                         <IonInput type='email' placeholder='Email Address' clearInput></IonInput>
                         <IonInput type='password' placeholder='Password' clearInput></IonInput>
                         <IonInput type='password' placeholder='Confirm Password' clearInput></IonInput>
-                        <IonItemDivider id='signup-photo-upload'>
+                        <IonItemDivider id='update-photo-upload'>
                             <IonLabel color='primary'>Add a Profile Photo</IonLabel>
                             <input type='file' id='imginput' accept='image/*'/>
                         </IonItemDivider>                        
-                        <IonButton id='signup-btn' fill='solid' color='primary'>Create Account</IonButton>           
+                        <IonButton id='update-btn' fill='solid' color='primary'>Create Account</IonButton>           
                     </IonCol>
                     <IonCol></IonCol>
                 </IonRow>
