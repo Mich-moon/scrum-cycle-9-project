d4adebeadcce63627f56a8176786e49e89d8bbdf
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 15:21:58 -0500

    update create event

--- templates/src/pages/CreateEvent.tsx
+++ templates/src/pages/CreateEvent.tsx
@@ -17,24 +17,6 @@ const CreateEvent: React.FC = () => {
                 </IonToolbar>
             </IonHeader> */}
 
-<<<<<<< Updated upstream
-            <IonGrid className='ion-margin' id='create-grid'>
-                <IonRow></IonRow>
-                <IonRow>
-                    <IonCol></IonCol>
-                    <IonCol id='create-col2'>
-                        <IonTitle className='ion-padding-bottom' id='create-title'>CREATE EVENT <i>to be updated</i></IonTitle>
-                        <IonInput placeholder='Firstname' clearInput></IonInput>
-                        <IonInput placeholder='Lastname' clearInput></IonInput>
-                        <IonInput type='email' placeholder='Email Address' clearInput></IonInput>
-                        <IonInput type='password' placeholder='Password' clearInput></IonInput>
-                        <IonInput type='password' placeholder='Confirm Password' clearInput></IonInput>
-                        <IonItemDivider id='create-photo-upload'>
-                            <IonLabel color='primary'>Add a Profile Photo</IonLabel>
-                            <input type='file' id='imginput' accept='image/*'/>
-                        </IonItemDivider>                        
-                        <IonButton id='create-btn' fill='solid' color='primary'>Create Account</IonButton>           
-=======
             <IonGrid className='ion-margin' id='createEvent-grid'>
                 <IonRow></IonRow>
                 <IonRow>
@@ -60,7 +42,6 @@ const CreateEvent: React.FC = () => {
                         </IonItemDivider>                     
                         <IonButton id='createEvent-btn' fill='solid' color='primary'>Create Event</IonButton>             
                                       
->>>>>>> Stashed changes
                     </IonCol>
                     <IonCol></IonCol>
                 </IonRow>
