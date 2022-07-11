da9945d4d6d6ce6bacafd218464ba3b9815b1bec
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 17:53:33 -0500

    Update App.tsx

--- templates/src/App.tsx
+++ templates/src/App.tsx
@@ -54,6 +54,9 @@ const App: React.FC = () => (
         <Route exact path='/main'>
             <Main />
         </Route>
+        <Route exact path='/profile'>
+            <Profile />
+        </Route>
         <Route exact path='/'>
           <Redirect to='/home' />
         </Route>
