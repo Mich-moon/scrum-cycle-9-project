f85275acb463d074fc39144fe91e8dcd03872bb8
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 11:42:49 -0500

    Updated header

--- templates/src/components/header.tsx
+++ templates/src/components/header.tsx
@@ -4,16 +4,16 @@ import { IonToolbar, IonButtons, IonButton } from '@ionic/react';
 interface ContainerProps { }
 
 const header: React.FC<ContainerProps> = () => {
-  return (
+  return (    
         <IonToolbar>
             <IonButtons id='home-btns' className='ion-justify-content-end'>
-                <IonButton id='home-btn1' color='primary' href='./home' target='_self'>
+                <IonButton id='home-btn1' href='./home' target='_self'>
                     Home
                 </IonButton>
-                <IonButton id='home-btn2' fill='outline' color='primary' href='./signup' target='_self'>
+                <IonButton id='home-btn2' fill='outline' href='./signup' target='_self'>
                     Signup
                 </IonButton>
-                <IonButton id='home-btn3' fill='solid' color='primary' href='./login' target='_self'>
+                <IonButton id='home-btn3' fill='solid' href='./login' target='_self'>
                     Login
                 </IonButton>
             </IonButtons>
