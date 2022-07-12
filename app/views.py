248b02543e50540155a5345e26a524059698676a
Mich-moon <michaelawright20@yahoo.com>
2022-07-12 14:42:44 -0500

    edited

--- app/views.py
+++ app/views.py
@@ -566,7 +566,7 @@ def form_errors(form):
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8100')
+    response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', '*')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
