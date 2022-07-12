d3417ce813fd9affd0873e77a2194fd9ad9bb89f
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-11 21:40:30 -0500

    updated header and css

--- templates/src/components/userHeader.tsx
+++ templates/src/components/userHeader.tsx
@@ -6,18 +6,18 @@ interface ContainerProps { }
 const userHeader: React.FC<ContainerProps> = () => {
   return (    
         <IonToolbar color='primary'>
-            <IonButtons id='user-btns' class='ion-justify-content-end'>
+            <IonButtons id='user-btns' class='ion-justify-content-center'>
                 <IonButton id='user-btn1' href='./main' target='_self'>
                     Main
                 </IonButton>
                 <IonButton id='user-btn2' href='./createEvent' target='_self'>
                     Create Event
                 </IonButton>
-                <IonButton id='user-btn3' href='./home' target='_self'>
+                <IonButton id='user-btn3' href='./profile' target='_self'>
                     Profile
                 </IonButton>
                 {/* hover over profile button and it gives the name of the user*/}
-                <IonButton id='user-btn4' fill='outline' href='./home' target='_self'>
+                <IonButton id='user-btn4' href='./home' target='_self'>
                     Logout
                 </IonButton>
             </IonButtons>
