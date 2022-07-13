0a5994aa7183031a3f9ea82c106f9572bb468412
Mich-moon <michaelawright20@yahoo.com>
2022-07-13 15:34:51 -0500

    comments

--- templates/src/App.tsx
+++ templates/src/App.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact, useIonViewWillEnter } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+import { useState } from 'react';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -33,52 +34,108 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path='/home'>
-          <Home />
-        </Route>
-        <Route exact path='/'>
-          <Redirect to='/home' />
-        </Route>
-        <Route exact path='/signup'>
-            <Signup />
-        </Route>
-        <Route exact path='/login'>
-            <Login />
-        </Route>
-        <Route exact path='/createEvent'>
-            <CreateEvent />
-        </Route>
-        <Route exact path='/updateEvent'>
-            <UpdateEvent />
-        </Route>
-        <Route exact path='/viewEvent'>
-            <ViewEvent />
-        </Route>
-        <Route exact path='/main'>
-            <Main />
-        </Route>
-        <Route exact path='/profile'>
-            <Profile />
-        </Route>
-         <Route exact path='/updateProfile'>
-            <UpdateProfile />
-        </Route>
-        <Route exact path='/addAdmin'>
-            <AddAdmin />
-        </Route>
-        <Route exact path='/ViewUser'>
-            <ViewUser />
-        </Route>
-        <Route exact path='/'>
-          <Redirect to='/home' />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+
+interface User {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  photo: string;
+  role: string;
+  created_at: string;
+}
+
+const App: React.FC = () => {
+
+    const [user, setUser] = useState<User>();
+
+    useIonViewWillEnter(() => {
+      getUser();
+
+      async function getUser() {
+        // get token from capacitor storage
+        //const jwt_value  = await Storage.get({ key: 'jwt' });
+        //const token = jwt_value;
+        //console.log(jwt_value);
+
+        let token = localStorage.getItem("jwt");
+        let user_id = localStorage.getItem("user_id");
+
+        const result = await fetch("http://localhost:8080/api/v2/events/users/" + user_id, {
+            method: "get",
+            headers: {
+                "Authorization" : "Bearer " + token
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        if ("user" in result) {
+            setUser(result.user);
+        }
+      }
+    }, [])
+
+    return (
+      <IonApp>
+        <IonReactRouter>
+          <IonRouterOutlet>
+            <Route exact path='/home'>
+              <Home />
+            </Route>
+            <Route exact path='/'>
+              <Redirect to='/home' />
+            </Route>
+            <Route exact path='/signup'>
+                <Signup />
+            </Route>
+            <Route exact path='/login'>
+                <Login />
+            </Route>
+            <Route exact path='/createEvent'>
+                <CreateEvent />
+            </Route>
+            <Route exact path='/updateEvent'>
+                <UpdateEvent />
+            </Route>
+            <Route exact path='/viewEvent'>
+                <ViewEvent />
+            </Route>
+            <Route exact path='/main'>
+                <Main />
+            </Route>
+            <Route exact path='/profile'>
+                <Profile 
+                    id= "{user.id}"
+                    first_name= "{user.first_name}"
+                    last_name= "{user.last_name}"
+                    email= "{user.email}"
+                    photo= "{user.photo}"
+                    role= "{user.role}"
+                    created_at= "{user.created_at}"
+                  />
+                {/*<Profile/>*/}
+            </Route> 
+            <Route exact path='/updateProfile'>
+                <UpdateProfile />
+            </Route>
+            <Route exact path='/addAdmin'>
+                <AddAdmin />
+            </Route>
+            <Route exact path='/ViewUser'>
+                <ViewUser />
+            </Route>
+            <Route exact path='/'>
+              <Redirect to='/home' />
+            </Route>
+          </IonRouterOutlet>
+        </IonReactRouter>
+      </IonApp>
+    );
+};
 
 export default App;
--- templates/src/pages/Profile.tsx
+++ templates/src/pages/Profile.tsx
@@ -1,60 +1,92 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonLabel, IonText, IonItemDivider, IonPage, IonRow, IonAvatar, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonLabel, IonText, IonItemDivider, IonPage, IonRow, IonAvatar, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { useState } from 'react';
 import Header from '../components/userHeader';
 import AdminHeader from '../components/adminHeader';
 import './Profile.css';
 
-const Profile: React.FC = () => {
-  return (
-    <IonPage>
-        <IonHeader className='ion-no-border'>
-            <IonToolbar>
-                <Header />
-                <AdminHeader />
-                {/* only show admin header if the user role is admin */}
-            </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        {/*   <IonHeader collapse='condense'>
-                <IonToolbar>
-                    <IonTitle size='large'>Blank2</IonTitle>
-                </IonToolbar>
-            </IonHeader> */}
+interface User {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    photo: string;
+    role: string;
+    created_at: string;
+}
 
-            <IonGrid className='ion-margin' id='profile-grid'>
-                <IonRow></IonRow>
-                <IonRow id='profile-row2'>
-                    
-                    <IonCol id='profile-col2'>
-                       {/* <IonTitle className='ion-padding-bottom' id='profile-title-right'>Signup</IonTitle>*/}
-                       <IonAvatar class= "center">
-                            <img id='profile-img'   src={process.env.PUBLIC_URL + '/assets/images/signup_image.png'} alt='profile photo'/>
-                       </IonAvatar>
-                       <IonItemDivider id='field'>
-                            <IonLabel color="light">Firstname:</IonLabel>
-                            <IonText> Firstname</IonText>
-                        </IonItemDivider>
+const Profile: React.FC<User> = (props:User) => {
+
+    const [isAdmin, setRole] = useState<string | null>('false');
+
+    useIonViewWillEnter(() => {
+
+        getClaims();
+
+        async function getClaims() {
+            // get role from capacitor storage
+            //const user_is_admin_value = await Storage.get({ key: 'user_is_admin' });
+            //const user_is_admin = user_is_admin_value;
+            //console.log( user_is_admin );
+
+            let user_is_admin = localStorage.getItem("user_is_admin");
+            setRole(user_is_admin);
+            console.log(user_is_admin);
+
+        }
+
+    }, [])
+
+    return (
+        <IonPage>
+            <IonHeader className='ion-no-border'>
+                <IonToolbar>
+                    {isAdmin == "false" && <Header />}
+                    {isAdmin == "true" && <AdminHeader />}
+                    {/* only show admin header if the user role is admin */}
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+            {/*   <IonHeader collapse='condense'>
+                    <IonToolbar>
+                        <IonTitle size='large'>Blank2</IonTitle>
+                    </IonToolbar>
+                </IonHeader> */}
+
+                <IonGrid className='ion-margin' id='profile-grid'>
+                    <IonRow></IonRow>
+                    <IonRow id='profile-row2'>
+                        
+                        <IonCol id='profile-col2'>
+                        {/* <IonTitle className='ion-padding-bottom' id='profile-title-right'>Signup</IonTitle>*/}
+                        <IonAvatar class= "center">
+                                <img id='profile-img'   src={process.env.PUBLIC_URL + '/assets/images/signup_image.png'} alt='profile photo'/>
+                        </IonAvatar>
                         <IonItemDivider id='field'>
-                            <IonLabel color="light">Lastname:</IonLabel>
-                            <IonText> Lastname</IonText>
-                        </IonItemDivider>
-                        <IonItemDivider id='field'>
-                            <IonLabel color="light">Email:</IonLabel>
-                            <IonText>Email </IonText>
-                        </IonItemDivider>
-                        <IonItemDivider id='field'>
-                            <IonLabel color="light">Password:</IonLabel>
-                            <IonText>Password</IonText>
-                        </IonItemDivider> 
-                        <IonItemDivider id="edit-btn-div">
-                             <IonButton id='profile-btn2' fill='outline' color={'white'} href="./updateProfile">Delete</IonButton>
-                             <IonButton id='profile-btn' fill='solid' href="./updateProfile">Edit</IonButton>
-                        </IonItemDivider>
-                    </IonCol> 
-                </IonRow> 
-            </IonGrid>
-        </IonContent>
-    </IonPage>
-  );
+                                <IonLabel color="light">Firstname:</IonLabel>
+                                <IonText> { props.first_name } </IonText>
+                            </IonItemDivider>
+                            <IonItemDivider id='field'>
+                                <IonLabel color="light">Lastname:</IonLabel>
+                                <IonText> { props.last_name } </IonText>
+                            </IonItemDivider>
+                            <IonItemDivider id='field'>
+                                <IonLabel color="light">Email:</IonLabel>
+                                <IonText> {props.email} </IonText>
+                            </IonItemDivider>
+                            <IonItemDivider id='field'>
+                                <IonLabel color="light">Password:</IonLabel>
+                                <IonText>Password</IonText>
+                            </IonItemDivider> 
+                            <IonItemDivider id="edit-btn-div">
+                                <IonButton id='profile-btn2' fill='outline' color={'white'} href="./updateProfile">Delete</IonButton>
+                                <IonButton id='profile-btn' fill='solid' href="./updateProfile">Edit</IonButton>
+                            </IonItemDivider>
+                        </IonCol> 
+                    </IonRow> 
+                </IonGrid>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Profile;
