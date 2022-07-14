2cfaee7419a714cf2ca77aaac9f270d563e52d08
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2022-07-13 23:34:33 -0500

    Update headers too accommodate mobile view

--- templates/src/components/adminHeader.tsx
+++ templates/src/components/adminHeader.tsx
@@ -1,28 +1,55 @@
 import './adminHeader.css';
-import { IonToolbar, IonButtons, IonButton } from '@ionic/react';
+import { IonToolbar, IonButtons, IonButton, IonContent, IonIcon, IonItem, IonList, IonPopover } from '@ionic/react';
+import { menu } from 'ionicons/icons';
+import { useRef, useState } from 'react';
 
 interface ContainerProps { }
 
-const adminHeader: React.FC<ContainerProps> = () => {
+const AdminHeader: React.FC<ContainerProps> = () => {
+    
+    const popover = useRef<HTMLIonPopoverElement>(null);
+    const [popoverOpen, setPopoverOpen] = useState(false);
+  
+    const openPopover = (e: any) => {
+      popover.current!.event = e;
+      setPopoverOpen(true);
+    };
+
   return (
-    <IonToolbar color='primary'>
-            <IonButtons id='admin-btns' class='ion-justify-content-center'>
-                <IonButton id='admin-btn1' href='./main' target='_self'>
-                    Main
-                </IonButton>
-                <IonButton id='admin-btn2' href='./addAdmin' target='_self'>
-                    Add Admin
-                </IonButton>
-                <IonButton id='admin-btn3' href='./profile' target='_self'>
-                    Profile
-                </IonButton>
-                {/* hover over profile button and it gives the name of the user*/}
-                <IonButton id='admin-btn4' fill='solid' href='./home' target='_self'>
-                    Logout
-                </IonButton>
-            </IonButtons>
-        </IonToolbar>
+    <><IonToolbar color='primary' className='ion-hide-lg-down'>
+          <IonButtons id='admin-btns' class='ion-justify-content-center'>
+              <IonButton id='admin-btn1' href='./main' target='_self'>
+                  Main
+              </IonButton>
+              <IonButton id='admin-btn2' href='./addAdmin' target='_self'>
+                  Add Admin
+              </IonButton>
+              <IonButton id='admin-btn3' href='./profile' target='_self'>
+                  Profile
+              </IonButton>
+              {/* hover over profile button and it gives the name of the user*/}
+              <IonButton id='admin-btn4' fill='solid' href='./home' target='_self'>
+                  Logout
+              </IonButton>
+          </IonButtons>
+      </IonToolbar>
+      
+      <IonToolbar id='mobile-menu' className='ion-hide-lg-up'>
+              <IonButton onClick={openPopover} id='menu-btn'>
+                  <IonIcon icon={menu}></IonIcon>
+              </IonButton>
+              <IonPopover ref={popover} isOpen={popoverOpen} onDidDismiss={() => setPopoverOpen(false)}>
+                  <IonContent>
+                      <IonList id='menu-items'>
+                          <IonItem href='./main' target='_self'>Home</IonItem>
+                          <IonItem href='./addAdmin' target='_self'>Add New Admin</IonItem>
+                          <IonItem href='./profile' target='_self'>Profile</IonItem>
+                          <IonItem href='./home' target='_self'>Logout</IonItem>
+                      </IonList>
+                  </IonContent>
+              </IonPopover>
+          </IonToolbar></>
   );
 };
 
-export default adminHeader;
+export default AdminHeader;
--- templates/src/components/header.tsx
+++ templates/src/components/header.tsx
@@ -1,24 +1,48 @@
 import './header.css';
-import { IonToolbar, IonButtons, IonButton } from '@ionic/react';
+import React, { useRef, useState } from 'react';
+import { IonButton, IonButtons, IonContent, IonIcon, IonItem, IonList, IonPopover, IonToolbar } from '@ionic/react';
+import { menu } from 'ionicons/icons';
 
 interface ContainerProps { }
 
-const header: React.FC<ContainerProps> = () => {
+const Header: React.FC<ContainerProps> = () => {
+    const popover = useRef<HTMLIonPopoverElement>(null);
+    const [popoverOpen, setPopoverOpen] = useState(false);
+  
+    const openPopover = (e: any) => {
+      popover.current!.event = e;
+      setPopoverOpen(true);
+    };
+    
   return (    
-        <IonToolbar>
-            <IonButtons id='home-btns' className='ion-justify-content-end'>
-                <IonButton id='home-btn1' href='./home' target='_self'>
-                    Home
-                </IonButton>
-                <IonButton id='home-btn2' fill='outline' href='./signup' target='_self'>
-                    Signup
-                </IonButton>
-                <IonButton id='home-btn3' fill='solid' href='./login' target='_self'>
-                    Login
-                </IonButton>
-            </IonButtons>
-        </IonToolbar>
+        <><IonToolbar className='ion-hide-lg-down'>
+          <IonButtons id='home-btns' className='ion-justify-content-end'>
+              <IonButton id='home-btn1' href='./home' target='_self'>
+                  Home
+              </IonButton>
+              <IonButton id='home-btn2' fill='outline' href='./signup' target='_self'>
+                  Signup
+              </IonButton>
+              <IonButton id='home-btn3' fill='solid' href='./login' target='_self'>
+                  Login
+              </IonButton>
+          </IonButtons>
+      </IonToolbar>
+      <IonToolbar id='mobile-menu' className='ion-hide-lg-up'>
+              <IonButton onClick={openPopover} id='menu-btn'>
+                  <IonIcon icon={menu}></IonIcon>
+              </IonButton>
+              <IonPopover ref={popover} isOpen={popoverOpen} onDidDismiss={() => setPopoverOpen(false)}>
+                  <IonContent>
+                      <IonList id='menu-items'>
+                          <IonItem href='./home' target='_self'>Home</IonItem>
+                          <IonItem href='./signup' target='_self'>Signup</IonItem>
+                          <IonItem href='./login' target='_self'>Login</IonItem>
+                      </IonList>
+                  </IonContent>
+              </IonPopover>
+          </IonToolbar></>
   );
 };
 
-export default header;
+export default Header;
